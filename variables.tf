703385212224136ae3ff5421f8e82e48b1ebfa12
SrivalliAvala <srivalliavala999@gmail.com>
2024-12-06 20:37:57 +0530

    roboshop-infra

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "instances" {
+  type        = map
+  default     = {
+    mongodb = "t3.small"
+    redis = "t3.micro"
+    mysql = "t3.small"
+    rabbitmq = "t3.micro"
+    catalogue = "t3.micro"
+    user = "t3.micro"
+    cart = "t3.micro"
+    shipping = "t3.small"
+    payment = "t3.micro"
+    dispatch = "t3.micro"
+    frontend = "t3.micro"
+  }
+}
+
+variable "allow_all" {
+    type = string
+    default = "sg-0372233cbe1615ef2"
+}
+
+variable "zone_id" {
+    default = "Z075800523JQAT1TRA35I"
+}
+
+variable "domain_name" {
+    default = "dev-ops81s.online"
+}
